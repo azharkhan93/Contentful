6bbbb0c3a312193bb61853b98283cf1ad58da0b1
Azhar Khan <119923339+azharkhan93@users.noreply.github.com>
2023-10-19 14:58:42 +0530

    Graphql Updates

--- app/page.js
+++ app/page.js
@@ -2,63 +2,38 @@
 'use client'
 import Image from 'next/image'
 import { useState, useEffect } from 'react';
+import { useQuery, gql } from '@apollo/client';
+
 const contentful = require('contentful');
 
-const client = contentful.createClient({
-  space: 'qnuz52e7owhg',
-  environment: 'master',
-  accessToken: '-MeJwDDPY251bBfYRWgaRmfG4XLYqEbm2IDqJfV66Lc',
-});
+import client from '../lib/contentfulClient';
+
+const GET_ARTICLES = gql`
+  query GetArticles {
+    articleCollection {
+      items {
+        title
+      }
+    }
+  }
+`;
 export default function BlogEntry() {
-   const [entry, setEntry] = useState(null);
+  const { loading, error, data } = useQuery(GET_ARTICLES, { client });
 
-  useEffect(() => {
-    async function fetchEntry() {
-      try {
-        const response = await client.getEntry('2Y0hQfCK79wwpFscitFCn7');
-        setEntry(response.fields);
-      } catch (error) {
-        console.error('Error fetching Contentful entry:', error);
-      }
-    }
-
-    fetchEntry();
-  }, []);
-  useEffect(() => {
-    async function fetchImage() {
-      if (entry && entry.image) {
-        try {
-          const imageAsset = await client.getAsset(entry.image[0].sys.id);
-          setEntry((prevEntry) => ({
-            ...prevEntry,
-            image: imageAsset.fields.file.url,
-          }));
-        } catch (error) {
-          console.error('Error fetching image:', error);
-        }
-      }
-    }
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
   
-    fetchImage();
-  }, [entry])
+  
 
   return (
-    <div className="mt-2 flex flex-col items-center justify-center h-screen">
-  <h1 className=" text-5xl text-center mb-6 font-bold text-green-600">Contentful Entry</h1>
-  {entry && (
-    <div className="text-center">
-      <h2 className='text-2xl'>{entry.title}</h2>
-      <p className='text-xl mt-4'>{entry.post}</p>
-      {entry.image && (
-        <img
-        className='mt-4'
-          src={entry.image}
-          alt="Image Description"
-        />
-      )}
+    <div>
+      {data.articleCollection.items.map((article) => (
+        <div key={article.title}>
+          <h2>{article.title}</h2>
+          
+        </div>
+      ))}
     </div>
-  )}
-</div>
 
 
   )
